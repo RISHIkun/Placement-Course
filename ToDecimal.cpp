06c819c875982bb73d63319f3e0d7b16b904d27c
Rishi <rishi.sangare434@nmims.edu.in>
2022-05-24 12:30:20 +0530

    to decimal conversions

--- ToDecimal.cpp
+++ ToDecimal.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int binarytodecimal(int n)
 {
@@ -28,15 +28,41 @@ int octaltodecimal(int n)
     }
     return ans;
 }
+int hexadecimaltobinary(string n)
+{
+    int x=1;
+    int ans=0;
+   int s=n.size();
+   for (int i = s-1; i >=0; i--)
+   {
+      if(n[i]>='0'&&n[i]<='9')
+      {
+          ans+=x*(n[i]-'0');
+      }
+      else if(n[i]>='A'&&n[i]<='F')
+      {
+          ans+=x*(n[i]-'A'+10);
+      }
+      x*=16;
+   }
+   
+
+    return ans;
+}
 
 int main()
 {
 int n,n2;
+string n3;
 cout<<"Enter binary num to convert into decimal"<<endl;
 cin>>n;
 cout<<binarytodecimal(n)<<endl;
 cout<<"Enter octal num to convert into decimal"<<endl;
 cin>>n2;
 cout<<octaltodecimal(n2)<<endl;
+cout<<"Enter the hecadecimal num to convert into decimal"<<endl;
+cin>>n3;
+cout<<hexadecimaltobinary(n3);
 return 0;
+
 }
