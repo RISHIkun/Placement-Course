0deee98a2e6c1681a4172c9b6c0dcba6ef40b5e6
Rishi <rishi.sangare434@nmims.edu.in>
2022-05-24 11:37:17 +0530

    octal to decimal

--- binarytodecimal.cpp
+++ binarytodecimal.cpp
@@ -14,10 +14,29 @@ int binarytodecimal(int n)
     }
     return ans;
 }
+int octaltodecimal(int n)
+{
+    int x=1;
+    int ans=0;
+    while (n>0)
+    {
+        int lastdigit=n%10;
+        ans+=lastdigit*x;
+        x*=8;
+        n=n/10;
+
+    }
+    return ans;
+}
+
 int main()
 {
-int n;
+int n,n2;
+cout<<"Enter binary num to convert into decimal"<<endl;
 cin>>n;
-cout<<binarytodecimal(n);
+cout<<binarytodecimal(n)<<endl;
+cout<<"Enter octal num to convert into decimal"<<endl;
+cin>>n2;
+cout<<octaltodecimal(n2)<<endl;
 return 0;
 }
